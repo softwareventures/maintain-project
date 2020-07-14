efe55ac9aeb1d1f64a42f1e2d8c59b59d4eeaf9e
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-14 13:51:46 +0100

    style(prettier): apply prettier formatting

--- index.ts
+++ index.ts
@@ -31,19 +31,22 @@ export type Result = Success | NotDirectory | NotEmpty | YarnInstallFailed;
 
 export default async function init(destDir: string): Promise<Result> {
     const mkdir = fs.mkdir(destDir, {recursive: true});
-    const isDirectory = mkdir.then(() => true, reason => {
-        if (reason?.code === "EEXIST") {
-            return false;
-        } else {
-            throw reason;
+    const isDirectory = mkdir.then(
+        () => true,
+        reason => {
+            if (reason?.code === "EEXIST") {
+                return false;
+            } else {
+                throw reason;
+            }
         }
-    });
+    );
 
-    if (!await isDirectory) {
+    if (!(await isDirectory)) {
         return {type: "not-directory"};
     }
 
-    if (!await emptyDir(destDir)) {
+    if (!(await emptyDir(destDir))) {
         return {type: "not-empty"};
     }
 
@@ -63,25 +66,26 @@ export default async function init(destDir: string): Promise<Result> {
         gitInit(destDir)
     ])
         .then(allFn(result => result.type === "success"))
-        .then(success => success
-            ? yarnInstall(destDir)
-            : {type: "not-empty"});
+        .then(success => (success ? yarnInstall(destDir) : {type: "not-empty"}));
 }
 
 async function copy(source: string, destDir: string, destFile: string = source): Promise<Result> {
     const sourcePath = require.resolve("./template/" + source);
     const destPath = resolve(destDir, destFile);
 
-    return fs.mkdir(dirname(destPath), {recursive: true})
+    return fs
+        .mkdir(dirname(destPath), {recursive: true})
         .then(async () => fs.copyFile(sourcePath, destPath, constants.COPYFILE_EXCL))
-        .then(() => ({type: "success"}),
+        .then(
+            () => ({type: "success"}),
             reason => {
                 if (reason.code === "EEXIST") {
                     return {type: "not-empty"};
                 } else {
                     throw reason;
                 }
-            });
+            }
+        );
 }
 
 async function packageJson(destDir: string): Promise<Result> {
@@ -90,7 +94,8 @@ async function packageJson(destDir: string): Promise<Result> {
 
     const packageName = basename(dirname(destPath));
 
-    return fs.readFile(sourcePath, {encoding: "utf8"})
+    return fs
+        .readFile(sourcePath, {encoding: "utf8"})
         .then(text => JSON.parse(text))
         .then(json => ({
             ...json,
@@ -99,23 +104,63 @@ async function packageJson(destDir: string): Promise<Result> {
             bugs: `https://github.com/softwareventures/${packageName}`,
             repository: `github:softwareventures/${packageName}`
         }))
-        .then(json => formatPackageJson(json, {
-            keyOrder: ["private", "name", "version", "description", "keywords", "author", "maintainers",
-                "contributors", "homepage", "bugs", "repository", "license", "scripts", "main", "module", "browser",
-                "man", "preferGlobal", "bin", "files", "directories", "sideEffects", "types", "typings",
-                "dependencies", "optionalDependencies", "bundleDependencies", "bundledDependencies",
-                "peerDependencies", "devDependencies", "engines", "engine-strict", "engineStrict", "os", "cpu",
-                "eslintConfig", "prettier", "config", "ava", "release"]
-        }))
+        .then(json =>
+            formatPackageJson(json, {
+                keyOrder: [
+                    "private",
+                    "name",
+                    "version",
+                    "description",
+                    "keywords",
+                    "author",
+                    "maintainers",
+                    "contributors",
+                    "homepage",
+                    "bugs",
+                    "repository",
+                    "license",
+                    "scripts",
+                    "main",
+                    "module",
+                    "browser",
+                    "man",
+                    "preferGlobal",
+                    "bin",
+                    "files",
+                    "directories",
+                    "sideEffects",
+                    "types",
+                    "typings",
+                    "dependencies",
+                    "optionalDependencies",
+                    "bundleDependencies",
+                    "bundledDependencies",
+                    "peerDependencies",
+                    "devDependencies",
+                    "engines",
+                    "engine-strict",
+                    "engineStrict",
+                    "os",
+                    "cpu",
+                    "eslintConfig",
+                    "prettier",
+                    "config",
+                    "ava",
+                    "release"
+                ]
+            })
+        )
         .then(async text => fs.writeFile(destPath, text, {encoding: "utf8", flag: "wx"}))
-        .then(() => ({type: "success"}),
+        .then(
+            () => ({type: "success"}),
             reason => {
                 if (reason.code === "EEXIST") {
                     return {type: "not-empty"};
                 } else {
                     throw reason;
                 }
-            });
+            }
+        );
 }
 
 async function ideaProjectFiles(destDir: string): Promise<Result> {
@@ -131,21 +176,21 @@ async function ideaProjectFiles(destDir: string): Promise<Result> {
         .then(filterFn(path => path !== "modules.xml"));
 
     return sourcePaths
-        .then(mapFn(async path => {
-            const source = "idea.template" + sep + path;
-            const dest = ".idea" + sep + path;
+        .then(
+            mapFn(async path => {
+                const source = "idea.template" + sep + path;
+                const dest = ".idea" + sep + path;
 
-            return copy(source, destDir, dest);
-        }))
-        .then(append([
-            copy("idea.template/create-project.iml", destDir, `.idea/${packageName}.iml`)
-        ]))
+                return copy(source, destDir, dest);
+            })
+        )
+        .then(
+            append([copy("idea.template/create-project.iml", destDir, `.idea/${packageName}.iml`)])
+        )
         .then(append([ideaModulesXml(destDir)]))
         .then(async results => Promise.all(results))
         .then(allFn(result => result.type === "success"))
-        .then(success => success
-            ? {type: "success"}
-            : {type: "not-empty"});
+        .then(success => (success ? {type: "success"} : {type: "not-empty"}));
 }
 
 async function ideaModulesXml(destDir: string): Promise<Result> {
@@ -157,33 +202,49 @@ async function ideaModulesXml(destDir: string): Promise<Result> {
     const dom = xmlText.then(xmlText => new JSDOM(xmlText, {contentType: "application/xml"}));
     const document = dom.then(dom => dom.window.document);
 
-    const module = document.then(document => document.querySelector("project:root>component>modules>module"))
+    const module = document
+        .then(document => document.querySelector("project:root>component>modules>module"))
         .then(nonNull);
 
     const newXmlText = module
         .then(module => {
-            module.setAttribute("fileurl", nonNull(module.getAttribute("fileurl"))
-                .replace(/create-project\.iml$/, packageName + ".iml"));
-            module.setAttribute("filepath", nonNull(module.getAttribute("filepath"))
-                .replace(/create-project\.iml$/, packageName + ".iml"));
+            module.setAttribute(
+                "fileurl",
+                nonNull(module.getAttribute("fileurl")).replace(
+                    /create-project\.iml$/,
+                    packageName + ".iml"
+                )
+            );
+            module.setAttribute(
+                "filepath",
+                nonNull(module.getAttribute("filepath")).replace(
+                    /create-project\.iml$/,
+                    packageName + ".iml"
+                )
+            );
         })
         .then(async () => dom)
         .then(dom => dom.serialize());
 
     return newXmlText
-        .then(async newXmlText => fs.writeFile(destPath, newXmlText, {encoding: "utf8", flag: "wx"}))
-        .then(() => ({type: "success"}),
+        .then(async newXmlText =>
+            fs.writeFile(destPath, newXmlText, {encoding: "utf8", flag: "wx"})
+        )
+        .then(
+            () => ({type: "success"}),
             reason => {
                 if (reason.code === "EEXIST") {
                     return {type: "not-empty"};
                 } else {
                     throw reason;
                 }
-            });
+            }
+        );
 }
 
 async function dictionary(destDir: string): Promise<Result> {
-    const words = fs.readFile(require.resolve("./template/dictionary.txt"), "utf8")
+    const words = fs
+        .readFile(require.resolve("./template/dictionary.txt"), "utf8")
         .then(words => words.split("\n"));
 
     const dom = new JSDOM("<component/>", {contentType: "application/xml"});
@@ -203,45 +264,53 @@ async function dictionary(destDir: string): Promise<Result> {
         .then(filterFn(word => word !== ""))
         .then(mapFn(word => word.trim()))
         .then(words => words.sort())
-        .then(mapFn(word => {
-            const element = document.createElement("w");
-            element.textContent = word;
-            wordsElement.appendChild(element);
-        }));
+        .then(
+            mapFn(word => {
+                const element = document.createElement("w");
+                element.textContent = word;
+                wordsElement.appendChild(element);
+            })
+        );
 
     const xmlText = wordElements
         .then(() => dom.serialize())
-        .then(xmlText => formatXml(xmlText, {
-            collapseContent: true,
-            indentation: "  ",
-            stripComments: true
-        }));
+        .then(xmlText =>
+            formatXml(xmlText, {
+                collapseContent: true,
+                indentation: "  ",
+                stripComments: true
+            })
+        );
 
     const destPath = resolve(destDir, ".idea/dictionaries/project.xml");
 
-    return fs.mkdir(dirname(destPath), {recursive: true})
+    return fs
+        .mkdir(dirname(destPath), {recursive: true})
         .then(async () => xmlText)
         .then(async xmlText => fs.writeFile(destPath, xmlText, {encoding: "utf8", flag: "wx"}))
-        .then(() => ({type: "success"}),
+        .then(
+            () => ({type: "success"}),
             reason => {
                 if (reason.code === "EEXIST") {
                     return {type: "not-empty"};
                 } else {
                     throw reason;
                 }
-            });
+            }
+        );
 }
 
 async function mkdir(path: string): Promise<Result> {
-    return fs.mkdir(path, {recursive: true})
-        .then(() => ({type: "success"}),
-            reason => {
-                if (reason.code === "EEXIST") {
-                    return {type: "not-empty"};
-                } else {
-                    throw reason;
-                }
-            });
+    return fs.mkdir(path, {recursive: true}).then(
+        () => ({type: "success"}),
+        reason => {
+            if (reason.code === "EEXIST") {
+                return {type: "not-empty"};
+            } else {
+                throw reason;
+            }
+        }
+    );
 }
 
 async function gitInit(destDir: string): Promise<Result> {
@@ -257,19 +326,19 @@ async function gitInit(destDir: string): Promise<Result> {
 
     const copyFiles = recursiveReadDir(templateDir)
         .then(mapFn(path => relative(templateDir, path)))
-        .then(mapFn(async path => {
-            const source = "git.template" + sep + path;
-            const dest = ".git" + sep + path;
+        .then(
+            mapFn(async path => {
+                const source = "git.template" + sep + path;
+                const dest = ".git" + sep + path;
 
-            return copy(source, destDir, dest);
-        }));
+                return copy(source, destDir, dest);
+            })
+        );
 
     return copyFiles
         .then(async copyFiles => Promise.all([...createDirectories, ...copyFiles]))
         .then(allFn(result => result.type === "success"))
-        .then(success => success
-            ? {type: "success"}
-            : {type: "not-empty"});
+        .then(success => (success ? {type: "success"} : {type: "not-empty"}));
 }
 
 async function yarnInstall(dir: string): Promise<Result> {
@@ -282,7 +351,8 @@ async function yarnInstall(dir: string): Promise<Result> {
                 } else {
                     resolve({type: "yarn-install-failed"});
                 }
-            }));
+            })
+    );
 }
 
 function main(destDir: string): void {
