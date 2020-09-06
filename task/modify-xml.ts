844c97e9158c98403133e038d2297fd2a2e8e394
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-09-06 22:18:06 +0100

    refactor(modify-xml): extract modify-xml task from writeIdeaModulesXml

---
+++ task/modify-xml.ts
@@ -0,0 +1,42 @@
+import {promises as fs} from "fs";
+import {dirname} from "path";
+import {JSDOM} from "jsdom";
+import {Result} from "./result";
+
+export interface Destination {
+    readonly destPath: string;
+}
+
+export async function modifyXml(
+    source: string,
+    modify: (document: Document) => Destination
+): Promise<Result> {
+    const sourcePath = require.resolve(`./template/${source}`);
+
+    const xmlText = fs.readFile(sourcePath, "utf8");
+    const dom = xmlText.then(xmlText => new JSDOM(xmlText, {contentType: "application/xml"}));
+    const document = dom.then(dom => dom.window.document);
+
+    const destPath = document.then(modify).then(({destPath}) => destPath);
+
+    const newXmlText = destPath.then(async () => dom).then(dom => dom.serialize());
+
+    return Promise.all([destPath, newXmlText])
+        .then(async ([destPath, newXmlText]) =>
+            fs
+                .mkdir(dirname(destPath), {recursive: true})
+                .then(async () =>
+                    fs.writeFile(destPath, newXmlText, {encoding: "utf8", flag: "wx"})
+                )
+        )
+        .then(
+            () => ({type: "success"}),
+            reason => {
+                if (reason.code === "EEXIST") {
+                    return {type: "not-empty"};
+                } else {
+                    throw reason;
+                }
+            }
+        );
+}
