2f85d8812540cca73973ee60b039006cd6da2ebc
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-07 23:46:04 +0100

    feat(init): create "init" subcommand, which creates a new project

--- cli/index.ts
+++ cli/index.ts
@@ -5,17 +5,20 @@ import {name, version} from "../package.json";
 import {cliInit} from "./init";
 
 export default function cli(): void {
-    new Command()
+    const program = new Command()
         .storeOptionsAsProperties(false)
         .passCommandToAction(false)
         .name(last(name.split("/")) ?? "")
-        .version(version)
-        .arguments("[destination]")
+        .version(version);
+
+    program
+        .command("init [destination]")
         .option("--scope <scope>")
         .option("--name <name>")
         .option("--github-owner <owner>")
         .option("--github-project <name>")
         .option("--webapp")
-        .action((destination, options) => cliInit(destination ?? cwd(), options))
-        .parse(argv);
+        .action((destination, options) => cliInit(destination ?? cwd(), options));
+
+    program.parse(argv);
 }
