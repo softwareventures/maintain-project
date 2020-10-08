521526b4e638aa3fd9324809d1b83428ee36e7c4
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-08 05:40:19 +0100

    feat(project): add readProject function
    Reads an existing project from disk and creates a Project object to represent it.

---
+++ project/read.ts
@@ -0,0 +1,35 @@
+import {promises as fs} from "fs";
+import {resolve} from "path";
+import {fromUrl as gitInfoFromUrl} from "hosted-git-info";
+import {Project} from "./project";
+
+export async function readProject(path: string): Promise<Project> {
+    path = resolve(path);
+
+    const packageJson = fs.readFile(resolve(path, "package.json"), "utf-8").then(JSON.parse);
+
+    const npmPackage = packageJson
+        .then(packageJson => packageJson.name ?? "")
+        .then(name => /^(?:(@.*?)\/)?(.*)$/.exec(name))
+        .then(([_, scope, name]) => ({scope, name}));
+
+    const githubProject = packageJson
+        .then(packageJson => packageJson.repository)
+        .then(gitInfoFromUrl)
+        .then(info =>
+            info.type === "github" ? {owner: info.user, name: info.project} : undefined
+        );
+
+    const target = fs
+        .stat(resolve(path, "webpack.config.js"))
+        .then(stats => (stats.isFile() ? "webapp" : "npm"));
+
+    return Promise.all([npmPackage, githubProject, target]).then(
+        ([npmPackage, githubProject, target]) => ({
+            path,
+            npmPackage,
+            githubProject,
+            target
+        })
+    );
+}
