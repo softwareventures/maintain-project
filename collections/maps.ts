68197f0675dee112974062c92010565aae1773b9
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-05 07:33:31 +0000

    refactor(maps): add insert function

--- collections/maps.ts
+++ collections/maps.ts
@@ -1,3 +1,7 @@
 export function copy<T, U>(map: ReadonlyMap<T, U>): Map<T, U> {
     return new Map(map);
 }
+
+export function insert<T, U>(map: ReadonlyMap<T, U>, key: T, value: U): Map<T, U> {
+    return copy(map).set(key, value);
+}
