9a8a75f1d1f17b2e272e6e7147dbac37328bc703
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-05 08:15:10 +0000

    refactor(fileset): add insertTextFile function

--- fileset/directory.ts
+++ fileset/directory.ts
@@ -1,51 +1,78 @@
 import {filter, head, isArray, tail} from "@softwareventures/array";
-import {insert} from "../collections/maps";
+import {insert as mapInsert} from "../collections/maps";
 import {TextFile} from "./text-file";
 
 export interface Directory {
     readonly type: "directory";
-    readonly entries: ReadonlyMap<string, Directory | TextFile>;
+    readonly entries: ReadonlyMap<string, DirectoryEntry>;
 }
+
+export type DirectoryEntry = Directory | TextFile;
 
 export const emptyDirectory: Directory = {type: "directory", entries: new Map()};
 
-export type InsertSubdirectoryResult = Directory | FileExists;
+export type InsertResult = Directory | FileExists;
 
 export interface FileExists {
     readonly type: "file-exists";
 }
 
-export function insertSubdirectory(
+export function insert(
     root: Directory,
-    path: string | readonly string[]
-): InsertSubdirectoryResult {
+    path: string | readonly string[],
+    entry: DirectoryEntry
+): InsertResult {
     const pathSegments = isArray(path) ? path : path.split("/");
-    return insertSubdirectoryInternal(
+    return insertInternal(
         root,
-        filter(pathSegments, segment => segment !== "")
+        filter(pathSegments, segment => segment !== ""),
+        entry
     );
 }
 
-function insertSubdirectoryInternal(
+export function insertSubdirectory(
     root: Directory,
-    path: readonly string[]
-): InsertSubdirectoryResult {
+    path: string | readonly string[]
+): InsertResult {
+    return insert(root, path, emptyDirectory);
+}
+
+export function insertTextFile(
+    root: Directory,
+    path: string | readonly string[],
+    text: string
+): InsertResult {
+    return insert(root, path, {type: "text-file", text});
+}
+
+function insertInternal(
+    root: Directory,
+    path: readonly string[],
+    entry: DirectoryEntry
+): InsertResult {
     const entryName = head(path);
 
     if (entryName == null) {
-        return root;
+        if (entry.type === "directory") {
+            return root;
+        } else {
+            return {type: "file-exists"};
+        }
     }
 
-    const entry = root.entries.get(entryName) ?? emptyDirectory;
+    const defaultEntry = path.length > 1 ? emptyDirectory : undefined;
+    const existingEntry = root.entries.get(entryName) ?? defaultEntry;
 
-    if (entry.type === "directory") {
-        const subdirectory = insertSubdirectoryInternal(entry, tail(path));
-        if (subdirectory.type === "directory") {
-            return {type: "directory", entries: insert(root.entries, entryName, subdirectory)};
-        } else {
-            return subdirectory;
-        }
-    } else {
+    if (existingEntry != null && existingEntry.type !== "directory") {
         return {type: "file-exists"};
+    }
+
+    const newEntry =
+        existingEntry == null ? entry : insertInternal(existingEntry, tail(path), entry);
+
+    if (newEntry.type === "file-exists") {
+        return newEntry;
+    } else {
+        return {type: "directory", entries: mapInsert(root.entries, entryName, newEntry)};
     }
 }
