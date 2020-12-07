50f30b20209319040a60c421288cdd76581f3075
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-07 19:50:57 +0000

    refactor(iterators): add mapValue function

--- collections/maps.ts
+++ collections/maps.ts
@@ -2,6 +2,20 @@ export function copy<T, U>(map: ReadonlyMap<T, U>): Map<T, U> {
     return new Map(map);
 }
 
+export function mapValue<T, U, V>(map: ReadonlyMap<T, U>, f: (value: U, key: T) => V): Map<T, V> {
+    const result = new Map();
+    for (const [key, value] of map.entries()) {
+        result.set(key, f(value, key));
+    }
+    return result;
+}
+
+export function mapValueFn<T, U, V>(
+    f: (value: U, key: T) => V
+): (map: ReadonlyMap<T, U>) => Map<T, V> {
+    return map => mapValue(map, f);
+}
+
 export function insert<T, U>(map: ReadonlyMap<T, U>, key: T, value: U): Map<T, U> {
     return copy(map).set(key, value);
 }
