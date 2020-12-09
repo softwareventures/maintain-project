e808d108269687859d8d8f4b0baa69894e376f47
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-09 08:31:55 +0000

    refactor(promises): improve type signature of catchIf

--- promises/promises.ts
+++ promises/promises.ts
@@ -1,7 +1,15 @@
-export function catchIf<T, U = any>(
+export function catchIf<T, U>(
     predicate: (reason: any) => reason is U,
-    f: (reason: U) => T
-): (reason: any) => T {
+    f: (reason: U) => T | Promise<T>
+): (reason: any) => T | Promise<T>;
+export function catchIf<T>(
+    predicate: (reason: any) => boolean,
+    f: (reason: any) => T | Promise<T>
+): (reason: any) => T | Promise<T>;
+export function catchIf<T>(
+    predicate: (reason: any) => boolean,
+    f: (reason: any) => T | Promise<T>
+): (reason: any) => T | Promise<T> {
     return reason => {
         if (predicate(reason)) {
             return f(reason);
