ffffb41dbc513a3428fd0eec908228363d53b503
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-09 08:12:38 +0000

    refactor(maps): add liftPromises function

--- collections/maps.ts
+++ collections/maps.ts
@@ -1,3 +1,5 @@
+import {zip} from "./iterators";
+
 export function copy<T, U>(map: ReadonlyMap<T, U>): Map<T, U> {
     return new Map(map);
 }
@@ -18,4 +20,10 @@ export function mapValueFn<T, U, V>(
 
 export function insert<T, U>(map: ReadonlyMap<T, U>, key: T, value: U): Map<T, U> {
     return copy(map).set(key, value);
+}
+
+export async function liftPromises<T, U>(promises: ReadonlyMap<T, Promise<U>>): Promise<Map<T, U>> {
+    return Promise.all(promises.values())
+        .then(values => zip(promises.keys(), values))
+        .then(entries => new Map(entries));
 }
