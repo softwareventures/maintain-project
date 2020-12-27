2723f8d862c7d22d5ef3d82f591c5c8ca445bd60
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-27 12:25:58 +0000

    refactor(template): add filterIgnore function

---
+++ template/filter-ignore.ts
@@ -0,0 +1,17 @@
+import {promises as fs} from "fs";
+import {filterFn} from "@softwareventures/array";
+import {File} from "../fs-changeset/file";
+
+export async function filterIgnore(
+    source: string,
+    filter: (line: string) => boolean
+): Promise<File> {
+    const sourcePath = require.resolve(`./template/${source}`);
+    const text = fs.readFile(sourcePath, "utf-8");
+    const filteredText = text
+        .then(text => text.split(/\r?\n/))
+        .then(filterFn(filter))
+        .then(lines => lines.join("\n"));
+    const data = filteredText.then(text => new TextEncoder().encode(text));
+    return data.then(data => ({type: "file", data}));
+}
