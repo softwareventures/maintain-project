283d8771be3fafdd76f5636ca078eb5837b9ce3f
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-28 06:42:39 +0000

    refactor(fs-changeset): use Result monad

--- fs-changeset/commit.ts
+++ fs-changeset/commit.ts
@@ -2,19 +2,13 @@ import {resolve} from "path";
 import {promises as fs} from "fs";
 import chain from "@softwareventures/chain";
 import {mapFn} from "../collections/iterators";
-import {liftPromises, mapValueFn} from "../collections/maps";
-import {ignoreIf} from "../promises/promises";
-import {Success} from "../task/result";
+import {liftPromises, liftResults, mapValue} from "../collections/maps";
+import {mapAsyncResultFn, mapResultFn, Result} from "../result/result";
 import {Directory} from "./directory";
 import {FsChangeset} from "./fs-changeset";
 import {File} from "./file";
 
-export type CommitResult = Success | CommitFailure;
-
-export interface CommitFailure {
-    readonly type: "commit-failure";
-    readonly reasons: readonly CommitFailureReason[];
-}
+export type CommitResult = Result<CommitFailureReason>;
 
 export type CommitFailureReason = FileExists;
 
@@ -24,17 +18,10 @@ export interface FileExists {
 }
 
 export async function commit(path: string, changeset: FsChangeset): Promise<CommitResult> {
-    return open(path, changeset).then(async openResult => {
-        switch (openResult.type) {
-            case "open-fs-changeset":
-                return write(openResult).then(() => ({type: "success"}));
-            case "commit-failure":
-                return openResult;
-        }
-    });
+    return open(path, changeset).then(mapAsyncResultFn(write));
 }
 
-type OpenResult = OpenFsChangeset | CommitFailure;
+type OpenResult = Result<OpenFailureReason, OpenFsChangeset>;
 
 interface OpenFsChangeset {
     readonly type: "open-fs-changeset";
@@ -43,11 +30,13 @@ interface OpenFsChangeset {
 
 async function open(path: string, changeset: FsChangeset): Promise<OpenResult> {
     const {root, overwrite = false} = changeset;
-    return openDirectory({path, node: root, overwrite}).then(openRoot => ({
-        type: "open-fs-changeset",
-        path,
-        openRoot
-    }));
+    return openDirectory({path, node: root, overwrite}).then(
+        mapResultFn(openRoot => ({
+            type: "open-fs-changeset",
+            path,
+            openRoot
+        }))
+    );
 }
 
 interface OpenOptions<T> {
@@ -56,12 +45,20 @@ interface OpenOptions<T> {
     readonly overwrite: boolean;
 }
 
+type OpenNodeResult = Result<OpenFailureReason, OpenNode>;
+
 type OpenNode = OpenDirectory | OpenFile;
+
+type OpenFailureReason = FileExists;
+
+type OpenDirectoryResult = Result<OpenFailureReason, OpenDirectory>;
 
 interface OpenDirectory {
     readonly type: "open-directory";
     readonly entries: ReadonlyMap<string, OpenNode>;
 }
+
+type OpenFileResult = Result<OpenFailureReason, OpenFile>;
 
 interface OpenFile {
     readonly type: "open-file";
@@ -69,39 +66,49 @@ interface OpenFile {
     readonly data: ArrayBufferLike;
 }
 
-async function openDirectory(options: OpenOptions<Directory>): Promise<OpenDirectory> {
+async function openDirectory(options: OpenOptions<Directory>): Promise<OpenDirectoryResult> {
     const {overwrite} = options;
-    return fs
-        .mkdir(options.path)
-        .catch(
-            ignoreIf(
-                reason =>
-                    overwrite &&
-                    reason.code === "EEXIST" &&
-                    fs.stat(options.path).then(stat => stat.isDirectory())
-            )
-        )
-        .then(() => options.node.entries)
-        .then(
-            mapValueFn(async (node, relativePath) => {
-                const path = resolve(options.path, relativePath);
-                switch (node.type) {
-                    case "directory":
-                        return openDirectory({path, node, overwrite});
-                    case "file":
-                        return openFile({path, node, overwrite});
-                }
-            })
-        )
-        .then(liftPromises)
-        .then(entries => ({type: "open-directory", entries}));
+
+    try {
+        await fs.mkdir(options.path);
+    } catch (reason) {
+        if (reason.code !== "EEXIST") {
+            throw reason;
+        } else if (!overwrite || (await fs.stat(options.path).then(stat => !stat.isDirectory()))) {
+            return {type: "failure", reasons: [{type: "file-exists", path: options.path}]};
+        }
+    }
+
+    const openEntries = mapValue(options.node.entries, async (node, relativePath) => {
+        const path = resolve(options.path, relativePath);
+        switch (node.type) {
+            case "directory":
+                return openDirectory({path, node, overwrite});
+            case "file":
+                return openFile({path, node, overwrite});
+        }
+    });
+
+    return liftPromises<string, OpenNodeResult>(openEntries)
+        .then(liftResults)
+        .then(mapResultFn(entries => ({type: "open-directory", entries})));
 }
 
-async function openFile(options: OpenOptions<File>): Promise<OpenFile> {
+async function openFile(options: OpenOptions<File>): Promise<OpenFileResult> {
     const flags = options.overwrite ? "r+" : "wx+";
-    return fs
-        .open(options.path, flags)
-        .then(fileHandle => ({type: "open-file", fileHandle, data: options.node.data}));
+    return fs.open(options.path, flags).then(
+        fileHandle => ({
+            type: "success",
+            value: {type: "open-file", fileHandle, data: options.node.data}
+        }),
+        reason => {
+            if (reason.code === "EEXIST") {
+                return {type: "failure", reasons: [{type: "file-exists", path: options.path}]};
+            } else {
+                throw reason;
+            }
+        }
+    );
 }
 
 async function write(changeset: OpenFsChangeset): Promise<void> {
