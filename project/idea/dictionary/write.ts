093e652ef058d4b5a561bb3290c4a3cf4d3d27d1
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-12-29 11:27:31 +0000

    refactor(project/idea): implement writeIdeaDictionaries using FsChangeset

--- project/idea/dictionary/write.ts
+++ project/idea/dictionary/write.ts
@@ -1,12 +1,20 @@
-import {filterFn, mapFn} from "@softwareventures/array";
+import {filterFn, map, mapFn} from "@softwareventures/array";
 import chain from "@softwareventures/chain";
 import {JSDOM} from "jsdom";
 import formatXml = require("xml-formatter");
 import {textFile} from "../../../fs-changeset/file";
 import {FsChangeset, insert, InsertResult} from "../../../fs-changeset/fs-changeset";
+import {chainAsyncResultsFn} from "../../../result/result";
+import {Project} from "../../project";
 import {IdeaDictionary} from "./idea-dictionary";
 
-export function writeIdeaDictionary(
+export function writeIdeaDictionaries(
+    project: Project
+): (fsChangeset: FsChangeset) => Promise<InsertResult> {
+    return chainAsyncResultsFn(map(project.ideaProject?.dictionaries ?? [], writeIdeaDictionary));
+}
+
+function writeIdeaDictionary(
     dictionary: IdeaDictionary
 ): (fsChangeset: FsChangeset) => Promise<InsertResult> {
     const dom = new JSDOM("<component/>", {contentType: "application/xml"});
