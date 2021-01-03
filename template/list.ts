dc67e9d52e45924ade9ab4b7e44318936b6c59c8
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-01-03 09:00:27 +0000

    refactor(template): fix listTemplates to behave as expected
    Returns relative paths with POSIX separators ("/").

--- template/list.ts
+++ template/list.ts
@@ -1,7 +1,11 @@
-import {dirname, resolve} from "path";
+import {dirname, relative, resolve, sep} from "path";
+import {mapFn} from "@softwareventures/array";
 import recursiveReadDir = require("recursive-readdir");
 
 export async function listTemplates(path: string): Promise<string[]> {
     const templateDir = dirname(require.resolve("./template/index.ts"));
-    return recursiveReadDir(resolve(templateDir, path));
+    return recursiveReadDir(resolve(templateDir, path))
+        .then(mapFn(path => relative(templateDir, path)))
+        .then(mapFn(path => path.split(sep)))
+        .then(mapFn(path => path.join("/")));
 }
