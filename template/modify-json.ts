767a35a2700765188d47358ac122e74fe910450e
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-01-03 09:08:12 +0000

    refactor(template): fix incorrect path resolution in modifyJson function

--- template/modify-json.ts
+++ template/modify-json.ts
@@ -2,7 +2,7 @@ import {promises as fs} from "fs";
 import {File} from "../fs-changeset/file";
 
 export async function modifyJson<T = any>(source: string, modify: (json: T) => T): Promise<File> {
-    const sourcePath = require.resolve("./template/${source}");
+    const sourcePath = require.resolve(`./template/${source}`);
     const jsonText = fs.readFile(sourcePath, "utf-8");
     const jsonValue = jsonText.then(jsonText => JSON.parse(jsonText) as T);
     const newJsonValue = jsonValue.then(modify);
