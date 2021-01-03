648d1fd3d9bf50fe00f3c9fdafe8d07930004a74
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-01-03 11:06:10 +0000

    refactor(project/npm): reinstate missing formatPackageJson step in rewritten writePackageJson

--- project/npm/write.ts
+++ project/npm/write.ts
@@ -1,7 +1,9 @@
+import chain from "@softwareventures/chain";
+import {format as formatPackageJson} from "prettier-package-json";
 import {FsChangeset, insert, InsertResult} from "../../fs-changeset/fs-changeset";
 import {chainAsyncResultsFn} from "../../result/result";
 import {copy} from "../../template/copy";
-import {modifyJson} from "../../template/modify-json";
+import {modifyText} from "../../template/modify-text";
 import {bugsUrl, homepageUrl, repositoryShortcut} from "../git/git-host";
 import {Project} from "../project";
 
@@ -14,42 +16,105 @@ export function writeNpmFiles(
 function writePackageJson(project: Project): (fsChangeset: FsChangeset) => Promise<InsertResult> {
     const {npmPackage, gitHost} = project;
 
-    const file = modifyJson("package.json", json => ({
-        ...json,
-        name: npmPackage.scope ? `@${npmPackage.scope}/${npmPackage.name}` : npmPackage.name,
-        homepage: gitHost == null ? undefined : homepageUrl(gitHost),
-        bugs: gitHost == null ? undefined : bugsUrl(gitHost),
-        repository: gitHost == null ? undefined : repositoryShortcut(gitHost),
-        scripts: {
-            ...json.scripts,
-            build: project.target === "webapp" ? json.scripts.build : undefined,
-            prepare: project.target === "npm" ? json.scripts.prepare : undefined,
-            start: project.target === "webapp" ? json.scripts.start : undefined
-        },
-        dependencies: {
-            ...json.dependencies,
-            "@types/webpack-env":
-                project.target === "webapp" ? json.dependencies["@types/webpack-env"] : undefined
-        },
-        devDependencies: {
-            ...json.devDependencies,
-            "@softwareventures/tsconfig":
-                project.target === "npm"
-                    ? json.devDependencies["@softwareventures/tsconfig"]
-                    : undefined,
-            "@softwareventures/webpack-config":
-                project.target === "webapp"
-                    ? json.devDependencies["@softwareventures/webpack-config"]
-                    : undefined,
-            "ts-loader":
-                project.target === "webapp" ? json.devDependencies["ts-loader"] : undefined,
-            "webpack": project.target === "webapp" ? json.devDependencies.webpack : undefined,
-            "webpack-cli":
-                project.target === "webapp" ? json.devDependencies["webpack-cli"] : undefined,
-            "webpack-dev-server":
-                project.target === "webapp" ? json.devDependencies["webpack-dev-server"] : undefined
-        }
-    }));
+    const file = modifyText(
+        "package.json",
+        text =>
+            chain(text)
+                .map(JSON.parse)
+                .map(json => ({
+                    ...json,
+                    name: npmPackage.scope
+                        ? `@${npmPackage.scope}/${npmPackage.name}`
+                        : npmPackage.name,
+                    homepage: gitHost == null ? undefined : homepageUrl(gitHost),
+                    bugs: gitHost == null ? undefined : bugsUrl(gitHost),
+                    repository: gitHost == null ? undefined : repositoryShortcut(gitHost),
+                    scripts: {
+                        ...json.scripts,
+                        build: project.target === "webapp" ? json.scripts.build : undefined,
+                        prepare: project.target === "npm" ? json.scripts.prepare : undefined,
+                        start: project.target === "webapp" ? json.scripts.start : undefined
+                    },
+                    dependencies: {
+                        ...json.dependencies,
+                        "@types/webpack-env":
+                            project.target === "webapp"
+                                ? json.dependencies["@types/webpack-env"]
+                                : undefined
+                    },
+                    devDependencies: {
+                        ...json.devDependencies,
+                        "@softwareventures/tsconfig":
+                            project.target === "npm"
+                                ? json.devDependencies["@softwareventures/tsconfig"]
+                                : undefined,
+                        "@softwareventures/webpack-config":
+                            project.target === "webapp"
+                                ? json.devDependencies["@softwareventures/webpack-config"]
+                                : undefined,
+                        "ts-loader":
+                            project.target === "webapp"
+                                ? json.devDependencies["ts-loader"]
+                                : undefined,
+                        "webpack":
+                            project.target === "webapp" ? json.devDependencies.webpack : undefined,
+                        "webpack-cli":
+                            project.target === "webapp"
+                                ? json.devDependencies["webpack-cli"]
+                                : undefined,
+                        "webpack-dev-server":
+                            project.target === "webapp"
+                                ? json.devDependencies["webpack-dev-server"]
+                                : undefined
+                    }
+                }))
+                .map(json =>
+                    formatPackageJson(json, {
+                        keyOrder: [
+                            "private",
+                            "name",
+                            "version",
+                            "description",
+                            "keywords",
+                            "author",
+                            "maintainers",
+                            "contributors",
+                            "homepage",
+                            "bugs",
+                            "repository",
+                            "license",
+                            "scripts",
+                            "main",
+                            "module",
+                            "browser",
+                            "man",
+                            "preferGlobal",
+                            "bin",
+                            "files",
+                            "directories",
+                            "sideEffects",
+                            "types",
+                            "typings",
+                            "dependencies",
+                            "optionalDependencies",
+                            "bundleDependencies",
+                            "bundledDependencies",
+                            "peerDependencies",
+                            "devDependencies",
+                            "engines",
+                            "engine-strict",
+                            "engineStrict",
+                            "os",
+                            "cpu",
+                            "eslintConfig",
+                            "prettier",
+                            "config",
+                            "ava",
+                            "release"
+                        ]
+                    })
+                ).value
+    );
 
     return async fsChangeset => file.then(file => insert(fsChangeset, "package.json", file));
 }
---
+++ template/modify-text.ts
@@ -0,0 +1,10 @@
+import {promises as fs} from "fs";
+import {File} from "../fs-changeset/file";
+
+export async function modifyText(source: string, modify: (text: string) => string): Promise<File> {
+    const sourcePath = require.resolve(`./template/${source}`);
+    const text = fs.readFile(sourcePath, "utf-8");
+    const newText = text.then(modify);
+    const data = newText.then(newText => new TextEncoder().encode(newText));
+    return data.then(data => ({type: "file", data}));
+}
