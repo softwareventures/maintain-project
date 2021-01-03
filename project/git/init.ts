11f80c24d185f418ea4ec00804e8fb0735b4275c
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-01-03 06:22:15 +0000

    style: apply code style

--- project/git/init.ts
+++ project/git/init.ts
@@ -1,5 +1,10 @@
 import {mapFn} from "@softwareventures/array";
-import {FsChangeset, insertFn, InsertResult, insertSubdirectoryFn} from "../../fs-changeset/fs-changeset";
+import {
+    FsChangeset,
+    insertFn,
+    InsertResult,
+    insertSubdirectoryFn
+} from "../../fs-changeset/fs-changeset";
 import {asyncFn, liftFunctionFromPromise} from "../../promises/promises";
 import {chainAsyncResults, chainAsyncResultsFn, chainResultsFn} from "../../result/result";
 import {copy} from "../../template/copy";
@@ -7,17 +12,20 @@ import {listTemplates} from "../../template/list";
 
 export async function gitInit(fsChangeset: FsChangeset): Promise<InsertResult> {
     return chainAsyncResults(fsChangeset, [
-        asyncFn(chainResultsFn([
-            insertSubdirectoryFn(".git/objects/info"),
-            insertSubdirectoryFn(".git/objects/pack"),
-            insertSubdirectoryFn(".git/refs/heads"),
-            insertSubdirectoryFn(".git/refs/tags"),
-            insertSubdirectoryFn(".git/hooks")
-        ])),
-        liftFunctionFromPromise(listTemplates("git.template")
-            .then(mapFn(async path => copy(path)
-                .then(file => insertFn(`.git/${path}`, file))))
-            .then(mapFn(liftFunctionFromPromise))
-            .then(chainAsyncResultsFn))
+        asyncFn(
+            chainResultsFn([
+                insertSubdirectoryFn(".git/objects/info"),
+                insertSubdirectoryFn(".git/objects/pack"),
+                insertSubdirectoryFn(".git/refs/heads"),
+                insertSubdirectoryFn(".git/refs/tags"),
+                insertSubdirectoryFn(".git/hooks")
+            ])
+        ),
+        liftFunctionFromPromise(
+            listTemplates("git.template")
+                .then(mapFn(async path => copy(path).then(file => insertFn(`.git/${path}`, file))))
+                .then(mapFn(liftFunctionFromPromise))
+                .then(chainAsyncResultsFn)
+        )
     ]);
 }
