e37e5c1e3cbbf1edc47d31af9a138d534e293e59
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-14 20:41:24 +0100

    refactor(fs-changeset): remove data from user-facing File interface
    not used yet

--- fs-changeset/file.ts
+++ fs-changeset/file.ts
@@ -1,14 +1,10 @@
 export interface File {
     readonly type: "fs-changeset-file";
     readonly path: string;
-    readonly data: ArrayBufferLike;
 }
 
 export interface InternalFile extends File {
     readonly fullPath: string;
     readonly underlyingMTime: bigint | null;
-}
-
-export function file(path: string, data: ArrayBufferLike): File {
-    return {type: "fs-changeset-file", path, data};
+    readonly data: ArrayBufferLike;
 }
