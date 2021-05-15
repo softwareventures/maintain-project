af271b3ea424405b11f47d7c3cfe566013913169
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-15 20:52:26 +0100

    feat(cli): apply updates with "update" subcommand

--- cli/update.ts
+++ cli/update.ts
@@ -1,42 +1,38 @@
-// import {exit} from "process";
-// import chain from "@softwareventures/chain";
-// import {forEachFn, mapFn} from "@softwareventures/array";
-// import {readProject} from "../project/read";
-// import {update} from "../project/update";
-// import {bindFailureFn, mapResultFn} from "../result/result";
-// import {ReadFileFailureReason} from "../fs-stage/read-file-failure-reason";
-//
+import {exit} from "process";
+import chain from "@softwareventures/chain";
+import {forEachFn, mapFn} from "@softwareventures/array";
+import {readProject} from "../project/read";
+import {bindFailureFn} from "../result/result";
+import {updateProject} from "../project/update";
+
 export function cliUpdate(path: string, options: object): void {
-    // TODO
-    //     readProject(path)
-    //         .then(mapResultFn(update))
-    //         .then(
-    //             bindFailureFn(reasons => {
-    //                 chain(reasons)
-    //                     .map(
-    //                         mapFn((reason: ReadFileFailureReason): string => {
-    //                             switch (reason.type) {
-    //                                 case "invalid-path":
-    //                                     return `Invalid Path: ${reason.path}`;
-    //                                 case "not-a-directory":
-    //                                     return `Not a Directory: ${reason.path}`;
-    //                                 case "file-is-directory":
-    //                                     return `Expected a file but found a directory: ${reason.path}`;
-    //                                 case "file-not-found":
-    //                                     return `File Not Found: ${reason.path}`;
-    //                             }
-    //                         })
-    //                     )
-    //                     .map(forEachFn(message => console.error(`Error: ${message}`)));
-    //                 exit(1);
-    //             })
-    //         )
-    //         .catch(reason => {
-    //             if (!!reason && reason.message) {
-    //                 console.error(reason.message);
-    //             } else {
-    //                 console.error(reason);
-    //             }
-    //             exit(1);
-    //         });
+    readProject(path)
+        .then(updateProject)
+        .then(
+            bindFailureFn(reasons => {
+                chain(reasons)
+                    .map(
+                        mapFn(reason => {
+                            switch (reason.type) {
+                                case "not-a-directory":
+                                    return `Not a Directory: ${reason.path}`;
+                                case "file-exists":
+                                    return `File Exists: ${reason.path}`;
+                                case "git-not-clean":
+                                    return `Git working copy not clean: ${reason.path}`;
+                            }
+                        })
+                    )
+                    .map(forEachFn(message => console.error(`Error: ${message}`)));
+                exit(1);
+            })
+        )
+        .catch(reason => {
+            if (!!reason && reason.message) {
+                console.error(reason.message);
+            } else {
+                console.error(reason);
+            }
+            exit(1);
+        });
 }
