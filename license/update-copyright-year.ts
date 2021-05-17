a847d96fedef33cfb98676dd3a526c940cc55a4a
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-17 02:13:45 +0100

    fix(license): fix possible null in copyright year

--- license/update-copyright-year.ts
+++ license/update-copyright-year.ts
@@ -76,7 +76,9 @@ export async function updateCopyrightYear(project: Project): Promise<Update | nu
 
             const updatedCopyright = {
                 index: copyright.index,
-                text: `Copyright ${copyright.startYear}-${currentYear} ${assignee}`
+                text: `Copyright ${
+                    copyright.startYear ? `${copyright.startYear}-` : ""
+                }${currentYear} ${assignee}`
             };
 
             const updatedCopyrights = chain(tail(copyrights))
