55e25d875657bf41e192cf9c1e3b30a8b3e15922
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-06-08 10:58:44 +0100

    fix(license): fix ISC license assigning copyright of project to ISC

--- license/write.ts
+++ license/write.ts
@@ -1,8 +1,10 @@
 import spdxLicenseList = require("spdx-license-list/full");
+import {mapNullableFn, mapNullFn} from "@softwareventures/nullable";
 import {Project} from "../project/project";
 import {FsStage, insert, InsertResult} from "../fs-stage/fs-stage";
 import {success} from "../result/result";
 import {textFile} from "../fs-stage/file";
+import {readTemplateText} from "../template/read-text";
 
 export function writeLicense(project: Project): (fsStage: FsStage) => Promise<InsertResult> {
     const licenseExpression = project.license?.spdxLicense;
@@ -15,17 +17,31 @@ export function writeLicense(project: Project): (fsStage: FsStage) => Promise<In
         return async fsStage => success(fsStage);
     }
 
-    const text = spdxLicenseList[licenseExpression.licenseId]?.licenseText;
+    const text = readLicenseText(licenseExpression.licenseId);
 
-    if (text == null) {
-        return async fsStage => success(fsStage);
-    }
-
-    const file = textFile(
-        `Copyright ${project.license.year} ${project.license.copyrightHolder ?? ""}`.trim() +
-            "\n\n" +
-            text
+    const file = text.then(
+        mapNullableFn(text =>
+            textFile(
+                `Copyright ${project.license.year} ${
+                    project.license.copyrightHolder ?? ""
+                }`.trim() +
+                    "\n\n" +
+                    text
+            )
+        )
     );
 
-    return async fsStage => insert(fsStage, "LICENSE.md", file);
+    return async fsStage =>
+        file
+            .then(mapNullableFn(file => insert(fsStage, "LICENSE.md", file)))
+            .then(mapNullFn(() => success(fsStage)));
+}
+
+async function readLicenseText(licenseId: string): Promise<string | null> {
+    switch (licenseId) {
+        case "ISC":
+            return readTemplateText("LICENSE.ISC.md");
+        default:
+            return spdxLicenseList[licenseId]?.licenseText ?? null;
+    }
 }
