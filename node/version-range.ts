09eb47185805d447bac513bad1a7d61672f86809
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-06-16 16:50:51 +0100

    fix(node): compare node versions using semver instead of parsing them as floats

--- node/version-range.ts
+++ node/version-range.ts
@@ -1,10 +1,11 @@
 import chain from "@softwareventures/chain";
 import {initial, last, mapFn} from "@softwareventures/array";
-import {sortByFn} from "../collections/arrays";
+import {coerce, compare} from "semver";
+import {sortFn} from "../collections/arrays";
 
 export function nodeVersionRange(versions: readonly string[]): string {
     return chain(versions)
-        .map(sortByFn(parseFloat))
+        .map(sortFn<string>((a, b) => compare(coerce(a) ?? "0.0.0", coerce(b) ?? "0.0.0")))
         .map(mapFn(version => `^${version}`))
         .map(ranges => [...initial(ranges), last(ranges)?.replace(/^\^/, ">=")])
         .map(versions => versions.join(" || ")).value;
