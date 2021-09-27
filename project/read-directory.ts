026c13e8488cdf08220c8ebfa6c0460ba1c0c34a
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-27 16:43:26 +0100

    refactor(project): add readProjectDirectory function

---
+++ project/read-directory.ts
@@ -0,0 +1,7 @@
+import {Dirent, promises as fs} from "fs";
+import {resolve} from "path";
+import {ProjectSource} from "./project";
+
+export async function readProjectDirectory(project: ProjectSource, path: string): Promise<Dirent[]> {
+    return fs.readdir(resolve(project.path, path), {withFileTypes: true});
+}
