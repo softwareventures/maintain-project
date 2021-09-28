05c5c3bab79ee384247a8a7f40b9a1e9bbbb32d2
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-28 19:40:54 +0100

    fix(template): fix attempt to require.resolve file that doesn't exist

--- template/list.ts
+++ template/list.ts
@@ -3,7 +3,7 @@ import {mapFn} from "@softwareventures/array";
 import recursiveReadDir = require("recursive-readdir");
 
 export async function listTemplates(path: string): Promise<string[]> {
-    const templateDir = dirname(require.resolve("./template/index.js"));
+    const templateDir = dirname(require.resolve("./template/index.ts"));
     const relativeDir = resolve(templateDir, path);
     return recursiveReadDir(relativeDir)
         .then(mapFn(path => relative(relativeDir, path)))
