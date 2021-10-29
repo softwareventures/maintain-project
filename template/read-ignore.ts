4109503ba398685a869b5888d07d213329f91637
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-10-29 07:26:34 +0100

    fix(git): fix module not found when creating new project

--- template/read-ignore.ts
+++ template/read-ignore.ts
@@ -1,5 +1,8 @@
+import {basename, dirname} from "path";
+import {anyFn} from "@softwareventures/array";
+import {mapNullableFn, mapNullFn} from "@softwareventures/nullable";
 import {readIgnore} from "../ignore/read";
-import {success} from "../result/result";
+import {failure, success} from "../result/result";
 import {Ignore} from "../ignore/ignore";
 import {readTemplateDirectory} from "./read-directory";
 import {readTemplateText} from "./read-text";
@@ -8,6 +11,11 @@ export async function readTemplateIgnore(name: string): Promise<Ignore> {
     return readIgnore({
         path: name,
         readDirectory: async path => readTemplateDirectory(path),
-        readText: async path => readTemplateText(path).then(text => success(text))
+        readText: async path =>
+            readTemplateDirectory(dirname(path))
+                .then(anyFn(dirent => dirent.name === basename(path) && dirent.isFile()))
+                .then(exists => (exists ? readTemplateText(path) : null))
+                .then(mapNullableFn(text => success(text)))
+                .then(mapNullFn(() => failure([{type: "file-not-found", path}])))
     });
 }
