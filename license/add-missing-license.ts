b023460e61d0d3540336531b8cea2ff47f08aa26
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-11-08 21:24:43 +0000

    feat(license): don't add LICENSE.md to project if LICENSE or LICENSE.txt already exist

--- license/add-missing-license.ts
+++ license/add-missing-license.ts
@@ -1,16 +1,23 @@
+import {anyFn} from "@softwareventures/array";
 import {Project} from "../project/project";
 import {Update} from "../project/update";
 import {projectFileExists} from "../project/file-exists";
 import {writeLicense} from "./write";
 
 export async function addMissingLicense(project: Project): Promise<Update | null> {
-    return projectFileExists(project, "LICENSE.md").then(exists =>
-        exists
-            ? null
-            : {
-                  type: "fs-stage-update",
-                  log: "docs(license): add missing LICENSE.md",
-                  apply: writeLicense(project)
-              }
-    );
+    return Promise.all([
+        projectFileExists(project, "LICENSE"),
+        projectFileExists(project, "LICENSE.md"),
+        projectFileExists(project, "LICENSE.txt")
+    ])
+        .then(anyFn(exists => exists))
+        .then(exists =>
+            exists
+                ? null
+                : {
+                      type: "fs-stage-update",
+                      log: "docs(license): add missing LICENSE.md",
+                      apply: writeLicense(project)
+                  }
+        );
 }
