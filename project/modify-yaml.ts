22269f8fb8a28940e8abd4e766601ff50420618f
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-02-04 12:20:57 +0000

    refactor(project): shorten import

--- project/modify-yaml.ts
+++ project/modify-yaml.ts
@@ -1,8 +1,8 @@
 import {Document} from "yaml";
-import {ProjectSource} from "../project/project";
-import {readProjectYamlAsDocument, ReadYamlFailureReason} from "../project/read-yaml";
 import {mapResultFn, Result} from "../result/result";
 import {File, textFile} from "../fs-stage/file";
+import {ProjectSource} from "./project";
+import {readProjectYamlAsDocument, ReadYamlFailureReason} from "./read-yaml";
 
 export async function modifyProjectYaml(
     project: ProjectSource,
