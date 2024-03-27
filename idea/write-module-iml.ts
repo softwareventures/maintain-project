16971ea6f4f3db47d82569e3f8dc56dd7dc562c2
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-27 22:47:18 +0000

    fix(idea): delete obsolete automated modification of module iml file

--- idea/write-module-iml.ts
+++ idea/write-module-iml.ts
@@ -1,27 +1,15 @@
 import type {FsStage, InsertResult} from "../fs-stage/fs-stage";
 import {insert} from "../fs-stage/fs-stage";
-import {modifyTemplateXml} from "../template/modify-xml";
 import type {Project} from "../project/project";
 import {projectTemplateId} from "../template/project-template-id";
+import {copyFromTemplate} from "../template/copy";
 
 export function writeIdeaModuleIml(project: Project): (fsStage: FsStage) => Promise<InsertResult> {
     const imlFilename =
         project.target === "webapp" ? "template-webpack-project.iml" : "template-node-project.iml";
-    const file = modifyTemplateXml({
-        templateId: projectTemplateId(project),
-        pathSegments: [".idea", imlFilename],
-        modify: dom => {
-            const document = dom.window.document;
-
-            const excludeFolder = document.querySelector(
-                'module:root>component[name=NewModuleRootManager]>content>excludeFolder[url="file://$MODULE_DIR$/template/template"]'
-            );
-            excludeFolder?.parentNode?.removeChild(excludeFolder);
-
-            return dom;
-        }
-    });
 
     return async fsStage =>
-        file.then(file => insert(fsStage, `.idea/${project.npmPackage.name}.iml`, file));
+        copyFromTemplate(projectTemplateId(project), ".idea", imlFilename).then(file =>
+            insert(fsStage, `.idea/${project.npmPackage.name}.iml`, file)
+        );
 }
