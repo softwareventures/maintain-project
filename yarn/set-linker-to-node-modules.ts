251b811fb6938bba3994830f4d0246b8fbf5813a
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-28 16:16:19 +0000

    fix(yarn): use yarn CLI to set nodeLinker

--- yarn/set-linker-to-node-modules.ts
+++ yarn/set-linker-to-node-modules.ts
@@ -1,47 +1,18 @@
-import {map} from "@softwareventures/array";
-import type {Document} from "yaml";
 import type {ProjectSource} from "../project/project.js";
 import type {Result} from "../result/result.js";
-import {bindAsyncResultFn, bindFailureFn, failure, mapResultFn} from "../result/result.js";
-import type {ReadYamlFailureReason} from "../project/read-yaml.js";
-import {readProjectYamlAsDocument} from "../project/read-yaml.js";
-import type {WriteYamlFailureReason} from "../project/write-yaml.js";
-import {writeProjectYaml} from "../project/write-yaml.js";
+import {mapFailureFn} from "../result/result.js";
+import {yarn} from "./yarn.js";
 
 export type SetYarnLinkerResult = Result<SetYarnLinkerFailureReason>;
 
 export interface SetYarnLinkerFailureReason {
     readonly type: "set-yarn-linker-failed";
-    readonly reason: ReadYamlFailureReason | WriteYamlFailureReason;
 }
 
 export async function setYarnLinkerToNodeModules(
     project: ProjectSource
 ): Promise<SetYarnLinkerResult> {
-    return readProjectYamlAsDocument(project, ".yarnrc.yml")
-        .then(
-            mapResultFn(document => {
-                document.set("nodeLinker", "node-modules");
-                return document;
-            })
-        )
-        .then(
-            bindAsyncResultFn<ReadYamlFailureReason, WriteYamlFailureReason, Document.Parsed>(
-                async document => writeProjectYaml(project, ".yarnrc.yml", document)
-            )
-        )
-        .then(
-            bindFailureFn(reasons =>
-                failure(
-                    map(
-                        reasons,
-                        reason =>
-                            ({
-                                type: "set-yarn-linker-failed",
-                                reason
-                            }) as const
-                    )
-                )
-            )
-        );
+    return yarn(project, "config", "set", "nodeLinker", "node-modules").then(
+        mapFailureFn(() => ({type: "set-yarn-linker-failed"}) as const)
+    );
 }
