8e173d200b8caac624455a3ac141b883b8d0f12d
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-28 17:13:44 +0000

    fix: don't run CLI when importing index from another module
    This fixes hanging tests.

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+import {pathToFileURL} from "node:url";
 import cli from "./cli/index.js";
 
-cli();
+const execPath = process.argv[1];
+
+if (execPath != null && import.meta.url === pathToFileURL(execPath).href) {
+    cli();
+}
