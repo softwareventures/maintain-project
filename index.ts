d2105125d1c325c74653223c51f1fb33707482f7
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-28 17:22:48 +0000

    fix: fix CLI not running when it should

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 import {pathToFileURL} from "node:url";
+import {resolve} from "path";
 import cli from "./cli/index.js";
 
 const execPath = process.argv[1];
 
-if (execPath != null && import.meta.url === pathToFileURL(execPath).href) {
+if (
+    execPath != null &&
+    (import.meta.url === pathToFileURL(execPath).href ||
+        import.meta.url === pathToFileURL(resolve(execPath, "index.js")).href)
+) {
     cli();
 }
