c9462d223a946855b654356f759567e2b70af948
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-03-28 19:40:31 +0000

    fix: run cli even if importing from another module
    This check was not reliable. This project is not
    intended to be imported from another module anyway.
    And contrary to what I previously thought, this does
    not affect tests.

--- index.ts
+++ index.ts
@@ -1,14 +1,4 @@
 #!/usr/bin/env node
-import {pathToFileURL} from "url";
-import {resolve} from "path";
 import cli from "./cli/index.js";
 
-const execPath = process.argv[1];
-
-if (
-    execPath != null &&
-    (import.meta.url === pathToFileURL(execPath).href ||
-        import.meta.url === pathToFileURL(resolve(execPath, "index.js")).href)
-) {
-    cli();
-}
+cli();
